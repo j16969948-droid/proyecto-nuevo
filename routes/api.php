5f917f0c2ebbf22a244d8f56fb0e5071aaa8c2b4
yisuslopez45 <chuchodavidlopez@gmail.com>
2026-03-10 00:33:03 -0500

    feat: :sparkles: se agregan nuevas rutas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CatalogoServiciosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/v1/hola', function(){
-    return 'Hello World';
+
+
+Route::prefix('v1')->group(function () {
+    
+    Route::get('/hola', function(){
+        return 'Hello World';
+    });
+    
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+    });
+        
+        
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/logout', [AuthController::class, 'logout']);       
+    });
+
+    
+    Route::get('/catalogoServicios', [CatalogoServiciosController::class, 'index']);
+    
+
 });
