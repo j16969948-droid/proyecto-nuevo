de78913f4f54a01417fc91e3d59b87175ceb9da3
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2026-03-13 11:55:52 -0500

    feat: :sparkles: Update model to match with the db

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Servicio extends Model
 {
     protected $table = 'servicios';
+
+    const UPDATED_AT = null; // disable updated_at
+
+    protected $fillable = [
+        'nombre',
+        'slug',
+        'precio_usuario',
+        'precio_revendedor',
+        'estado',
+        'imagen',
+        'proveedor',
+        'telefono_proveedor'
+    ];
+
+    protected $casts = [
+        'precio_usuario' => 'decimal:2',
+        'precio_revendedor' => 'decimal:2',
+        'estado' => 'boolean'
+    ];
 }
