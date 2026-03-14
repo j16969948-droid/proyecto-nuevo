750f3588a6e2ffe5aee752c84b75bdf9ab53a3a0
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2026-03-14 13:27:41 -0500

    feat: :sparkles: Add management for PagoEmail

--- app/Http/Controllers/PagoEmailController.php
+++ app/Http/Controllers/PagoEmailController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PagoEmail;
-use Illuminate\Http\Request;
+use App\Http\Requests\PagoEmailCreateRequest;
+use App\Services\PagoEmailService;
 
 class PagoEmailController extends Controller
 {
-    public function index(){
-        return PagoEmail::all();
+    private $service;
+
+    public function __construct(PagoEmailService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        return response()->json(
+            $this->service->index()
+        );
+    }
+
+    public function show($id)
+    {
+        return response()->json(
+            $this->service->show($id)
+        );
+    }
+
+    public function store(PagoEmailCreateRequest $request)
+    {
+        return response()->json(
+            $this->service->create($request->validated())
+        );
     }
 }
--- app/Models/PagoEmail.php
+++ app/Models/PagoEmail.php
@@ -7,4 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class PagoEmail extends Model
 {
     protected $table = 'pagos_email';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+
+    const CREATED_AT = 'creado_en';
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'message_id',
+        'ordenante',
+        'monto',
+        'fecha_pago',
+        'hora_pago',
+        'usado',
+        'cliente_id_vinculado',
+        'pago_entrante_id',
+        'comprobante_url_vinculada',
+        'validado_por',
+        'validado_en'
+    ];
+
+    protected $casts = [
+        'monto' => 'decimal:2',
+        'fecha_pago' => 'date',
+        'hora_pago' => 'datetime:H:i:s',
+        'usado' => 'boolean',
+        'validado_en' => 'datetime',
+        'creado_en' => 'datetime'
+    ];
 }
