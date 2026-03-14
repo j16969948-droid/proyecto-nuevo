e911371df5f4478b70e1a3adaf368ad60d95fcef
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2026-03-14 17:54:10 -0500

    feat: :sparkles: Organize inventario model
    To match with the db

--- app/Models/Inventario.php
+++ app/Models/Inventario.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Inventario extends Model
 {
     protected $table = 'inventario';
-    public $timestamps = false;
+
+    public $timestamps = true;
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = null;
 
     protected $fillable = [
         'servicio_id',
@@ -20,6 +24,12 @@ class Inventario extends Model
         'telefono_asignado',
         'cliente_id_asignado',
         'estado',
+    ];
+
+    protected $casts = [
+        'fecha_compra' => 'date',
+        'fecha_vencimiento' => 'date',
+        'created_at' => 'datetime',
     ];
 
     public function servicio()
