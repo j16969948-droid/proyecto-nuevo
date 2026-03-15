115bb58cb00a63b11c9ba63e225fec738e835a2b
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2026-03-14 19:25:57 -0500

    feat: :sparkles: Improve Menu model
    To match with the db

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,22 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-   protected $table = 'system_menu';
+    protected $table = 'system_menu';
 
-   protected $fillable = [
-      'id_estado',
-   ];
+    protected $fillable = [
+        'nombre',
+        'url',
+        'logo',
+        'id_estado',
+    ];
 
-   public function submenus(){
-      return $this->hasMany(
-         SubMenu::class,
-         'id_menu',
-         'id'
-      );
-   }
+    public function submenus()
+    {
+        return $this->hasMany(
+            SubMenu::class,
+            'id_menu',
+            'id'
+        );
+    }
 
-   public function roles()
-   {
-      return $this->belongsToMany(Role::class, 'roles_menus', 'id_menu', 'id_rol');
-   }
+    public function roles()
+    {
+        return $this->belongsToMany(
+            Role::class,
+            'roles_menus',
+            'id_menu',
+            'id_rol'
+        );
+    }
 }
