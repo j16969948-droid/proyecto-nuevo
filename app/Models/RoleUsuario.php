5d1bf32a0e098aa1a122b293e8a6df524cfd7c84
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2026-03-14 19:33:46 -0500

    feat: :sparkles: Agregar modelo RoleUsuario con relaciones a Usuario y Rol

--- app/Models/RoleUsuario.php
+++ app/Models/RoleUsuario.php
@@ -8,4 +8,26 @@ class RoleUsuario extends Model
 {
     protected $table = 'roles_usuarios';
 
+    protected $fillable = [
+        'id_usuario',
+        'id_rol',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(
+            User::class,
+            'id_usuario',
+            'id'
+        );
+    }
+
+    public function rol()
+    {
+        return $this->belongsTo(
+            Role::class,
+            'id_rol',
+            'id'
+        );
+    }
 }
